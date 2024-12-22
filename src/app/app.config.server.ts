6500bb2b9f66f8b4a10e4074202be42288c0477a
Jurrit van der Ploeg <jurrit.vander.ploeg@ndwcloud.nu>
2024-12-22 23:36:53 +0100

    add firebase config to server

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,18 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
+
 import { APP_CONFIG } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { FIREBASE_CONFIG } from './firebase.config';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideServerRoutesConfig(serverRoutes)],
+  providers: [
+    provideFirebaseApp(() => initializeApp(FIREBASE_CONFIG)),
+    provideServerRendering(),
+    provideServerRoutesConfig(serverRoutes),
+  ],
 };
 
 export const config = mergeApplicationConfig(APP_CONFIG, serverConfig);
