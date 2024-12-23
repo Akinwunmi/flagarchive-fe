de070988590cfac54974c0a2bd20206af5b858b4
Jurrit van der Ploeg <jurritvanderploeg.dev@gmail.com>
2024-12-23 23:24:03 +0100

    feat(build): use @angular/builder (#30) (#31)

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBZzwMsFmhsl0g8BvCCsFrCpnEMawVkCeo',
+    appId: '1:1076024154430:web:bc11a9b5a6c9cd0ee5f46f',
+    authDomain: 'flagarchive-database.firebaseapp.com',
+    measurementId: 'G-169QS3QMGC',
+    messagingSenderId: '1076024154430',
+    projectId: 'flagarchive-database',
+    storageBucket: 'flagarchive-database.appspot.com',
+  },
+};
